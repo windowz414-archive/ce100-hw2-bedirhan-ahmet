8596ba9fc9d119d120b7e41eed6ee8a4f86dd7a7
Bushra KURT <bushra.kurt@outlook.com>
2023-03-31 14:11:39 +0300

    ce100-hw2-algo-test-cs: Write unit test for heap sort
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce100-hw2-algo-test-cs/ce100_hw2_algo_test_cs.cs
+++ ce100-hw2-algo-test-cs/ce100_hw2_algo_test_cs.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 using ce100_hw2_algo_lib_cs;
 
 namespace ce100_hw2_algo_test_cs
@@ -8,8 +9,22 @@ namespace ce100_hw2_algo_test_cs
     public class ce100_hw2_algo_test_cs
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestheapSort()
         {
+            Random rand = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                // Generate random input array
+                int[] input = Enumerable.Range(0, rand.Next(1, 1001)).Select(j => rand.Next(int.MinValue, int.MaxValue)).ToArray();
+
+                // Test heap sort algorithm
+                int[] actualOutput = new int[input.Length];
+                int result = ce100_hw2_algo_lib.heapSort(input, actualOutput);
+
+                Assert.AreEqual(0, result);
+            }
         }
+
     }
 }
